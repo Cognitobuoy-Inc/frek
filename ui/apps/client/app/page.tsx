f284079b63f2aa732333aed99ae2b40437eb12fd
ckacquah <49785034+ckacquah@users.noreply.github.com>
2023-05-20 12:55:25 +0000

    FREK-20 Add Instagram authentication

--- ui/apps/client/app/page.tsx
+++ ui/apps/client/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button, Header } from "ui";
+import { Button, Header, SignOutButton } from "ui";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <SignOutButton />
     </>
   );
 }
