439f4a5785350ac5b2acd83d8c8f7c0411c11a4f
ckacquah <49785034+ckacquah@users.noreply.github.com>
2023-05-20 12:55:25 +0000

    FREK-20 Add Instagram authentication

--- ui/packages/ui/Button.tsx
+++ ui/packages/ui/Button.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import * as React from "react";
+import { signIn, signOut } from "next-auth/react";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+  return <button onClick={() => signIn("instagram")}>SIGN IN INSTAGRAM</button>;
+};
+
+export const SignOutButton = () => {
+  return <button onClick={() => signOut()}>SIGN OUT</button>;
 };
