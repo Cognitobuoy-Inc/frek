bfcbac40d4b9f4794a948751ff1ccb10f5a5e13b
ckacquah <49785034+ckacquah@users.noreply.github.com>
2023-05-20 14:46:55 +0000

    FREK-19 Add Facebook authentication

--- ui/apps/client/lib/auth.ts
+++ ui/apps/client/lib/auth.ts
@@ -1,4 +1,5 @@
 import { NextAuthOptions } from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
 import InstagramProvider from "next-auth/providers/instagram";
 
 export const authOptions: NextAuthOptions = {
@@ -6,6 +7,10 @@ export const authOptions: NextAuthOptions = {
     InstagramProvider({
       clientId: process.env.INSTAGRAM_CLIENT_ID,
       clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
 };
