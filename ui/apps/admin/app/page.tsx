566d6770fd10f9b9bffdb0a5ae4d77da9b0c592d
ckacquah <49785034+ckacquah@users.noreply.github.com>
2023-05-21 12:10:20 +0000

    FREK-23 Design build a landing page with the various authentication options

--- ui/apps/admin/app/page.tsx
+++ ui/apps/admin/app/page.tsx
@@ -3,7 +3,10 @@ import { Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Docs" />
+      <Header />
+      <p>
+        <h1>Admin page</h1>
+      </p>
     </>
   );
 }
--- ui/packages/ui/Button.tsx
+++ ui/packages/ui/Button.tsx
@@ -1,29 +1,78 @@
 "use client";
 
+import Button from "@mui/material/Button";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 import { signIn, signOut } from "next-auth/react";
+import { ButtonPropsColorOverrides } from "@mui/material";
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    google: true;
+    twitter: true;
+    facebook: true;
+    instagram: true;
+  }
+}
 
 export const GoogleSignInButton = () => {
-  return <button onClick={() => signIn("google")}>SIGN IN WITH GOOGLE</button>;
+  return (
+    <Button
+      variant="contained"
+      color="google"
+      startIcon={<GoogleIcon />}
+      onClick={() => signIn("google")}
+    >
+      SIGN IN WITH GOOGLE
+    </Button>
+  );
 };
 
 export const TwitterSignInButton = () => {
   return (
-    <button onClick={() => signIn("twitter")}>SIGN IN WITH TWITTER</button>
+    <Button
+      variant="contained"
+      color="twitter"
+      startIcon={<TwitterIcon />}
+      onClick={() => signIn("twitter")}
+    >
+      SIGN IN WITH TWITTER
+    </Button>
   );
 };
 
 export const FacebookSignInButton = () => {
   return (
-    <button onClick={() => signIn("facebook")}>SIGN IN WITH FACEBOOK</button>
+    <Button
+      variant="contained"
+      color="facebook"
+      startIcon={<FacebookIcon />}
+      onClick={() => signIn("facebook")}
+    >
+      SIGN IN WITH FACEBOOK
+    </Button>
   );
 };
 
 export const InstagramSignInButton = () => {
   return (
-    <button onClick={() => signIn("instagram")}>SIGN IN WITH INSTAGRAM</button>
+    <Button
+      variant="contained"
+      color="instagram"
+      startIcon={<InstagramIcon />}
+      onClick={() => signIn("instagram")}
+    >
+      SIGN IN WITH INSTAGRAM
+    </Button>
   );
 };
 
 export const SignOutButton = () => {
-  return <button onClick={() => signOut()}>SIGN OUT</button>;
+  return (
+    <Button variant="contained" onClick={() => signOut()}>
+      SIGN OUT
+    </Button>
+  );
 };
